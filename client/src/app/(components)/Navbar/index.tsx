6af3a45ca7d92e6610ba1de742c09ea15e23127f
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2024-08-20 21:54:32 +0800

    proceed to data modeling

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,23 +1,42 @@
 'use client'
 
-import { Bell, Menu, Settings, Sun } from 'lucide-react'
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state'
+import { Bell, Menu, Moon, Settings, Sun } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
-const Navbar = (props: Props) => {
+const Navbar = () => {
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode))
+  }
+
+  const sidebarClassNames = `fixed flex flex-col ${isSidebarCollapsed ? 'w-0 md:w-16' : 'w-72 md:w-64'} bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+
+
   return (
     <div className='flex justify-between items-center w-full mb-7'>
       {/* Left side */}
       <div className='flex justify-between items-center gap-5'>
-        <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => { }}>
+        <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toggleSidebar}>
           <Menu className='w-4 h-4' />
         </button>
 
         <div className='relative'>
           <input type='search' placeholder='Start type to search groups and products'
-            className='pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'
+            className='pl-10 pr-4 py-2 w-50 md:w-70 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'
           />
 
           <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
@@ -30,8 +49,14 @@ const Navbar = (props: Props) => {
       <div className='flex justify-between items-center gap-5'>
         <div className='hidden md:flex justify-between items-center gap-5'>
           <div>
-            <button onClick={() => { }}>
-              <Sun className='cursor-pointer text-gray-500' size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className='cursor-pointer text-gray-500' size={24} />
+
+              ) : (
+                <Moon className='cursor-pointer text-gray-500' size={24} />
+
+              )}
             </button>
           </div>
           <div className='relative'>
