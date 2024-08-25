438b91a4afc9232524844c71a833a34aeee80525
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2024-08-25 21:28:55 +0800

    image uploaded fix using.png

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -79,11 +79,11 @@ const Products = () => {
                         >
                             <div className="flex flex-col items-center">
                                 <Image
-                                    src={`https://s3-nventorymanagement.s3.ap-southeast-1.amazonaws.com/product1.png`}
+                                    src={`https://s3-nventorymanagement.s3.ap-southeast-1.amazonaws.com/product${Math.floor(Math.random() * 3) + 1}.png`}
                                     alt={product.name}
                                     width={150}
                                     height={150}
-                                    className='mb-3 rounded-2xl w-36 h-16'
+                                    className="mb-3 rounded-2xl w-36 h-36"
                                 />
                                 <h3 className="text-lg text-gray-900 font-semibold">
                                     {product.name}
